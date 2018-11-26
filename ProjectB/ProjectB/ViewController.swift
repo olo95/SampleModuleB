7b8de7ae529a9bc7b1f3a5c719aba1f68e0c43ad
Alexander Stolar <Alexander.Stolar@exence.com>
2018-11-26 12:34:11 +0100

    added xcode project

---
+++ ProjectB/ProjectB/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ProjectB
+//
+//  Created by Alexander Stolar on 26/11/2018.
+//  Copyright © 2018 Olo95. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
